9de2be7b5015f553d366dca5f748af3685596711
SaiKiranReddy456 <navarisaikiran435@gmail.com>
2025-09-17 08:15:28 +0530

    commit message

--- React-Project/src/Components/ContactUs/ContactUs.jsx
+++ React-Project/src/Components/ContactUs/ContactUs.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Form from "react-bootstrap/form";
+import Form from "react-bootstrap";
 import "./ContactUs.css";
 import { useState } from "react";
 import { toast } from "react-toastify";
